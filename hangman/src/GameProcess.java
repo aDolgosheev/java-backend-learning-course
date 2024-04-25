cce647cdaf58d8b5a5dc2f44a31f7f80ff287669
Aleksandr Dolgosheev <al.dolgosheev@gmail.com>
2024-04-25 14:50:41 +0300

    2024.04.25 hangman-game added

--- hangman/src/GameProcess.java
+++ hangman/src/GameProcess.java
@@ -1,17 +1,56 @@
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class GameProcess {
 
     static Scanner scanner = new Scanner(System.in);
 
-    public static void action() {
+    public void action() {
         Random random = new Random();
-        String hiddenWord = HangmanWordBank.WORDBANK.get(random.nextInt(HangmanWordBank.WORDBANK.size()));
+        String word = HangmanWordBank.WORDBANK.get(random.nextInt(HangmanWordBank.WORDBANK.size())).toUpperCase();
+        char[] hiddenWord = new char[word.length()];
+        Arrays.fill(hiddenWord, '_' );
 
-        System.out.println("Отлично! Давай начнем.");
-        System.out.println("Введите символ: ");
+        System.out.println("Отлично! Давайте начнем.");
 
+        Set<Character> lettersUsed = new HashSet<>();
+        int lives = 7;
+        while (lives > 0) {
+            System.out.println("Осталось попыток: " + lives);
+            System.out.println("\n" + printHiddenWord(hiddenWord) + "                  " + word); // слово указано справа в явном виде для проверки
+            System.out.println("\nВведите символ: ");
+            char letter = Character.toUpperCase(scanner.next().charAt(0));
+            if (!lettersUsed.add(letter)) {
+                System.out.println("Вы уже предлагали эту букву, выберите другую.");
+            } else if (word.contains(String.valueOf(letter))) {
+                lettersUsed.add(letter);
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == letter) {
+                        hiddenWord[i] = letter;
+                    }
+                }
+                if (!String.valueOf(hiddenWord).contains("_")) {
+                    System.out.println("Победа!");
+                    System.out.println("Загаданное слово: \n" + printHiddenWord(hiddenWord));
+                    return;
+                }
+            } else {
+                lettersUsed.add(letter);
+                System.out.println(HangmanWordBank.HANGMANPICS.get(lives - 1));
+                System.out.println("Такой буквы нет в слове.");
+                lives--;
+            }
+            System.out.println("Вы использовали следующие буквы:\n" +
+                    lettersUsed.toString());
+        }
+        System.out.println("Вы проиграли!\n" +
+                "Загаданное слово было: " + word);
+    }
+
+    public String printHiddenWord(char[] hiddenWord) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char letter : hiddenWord) {
+            stringBuilder.append(letter);
+        }
+        return stringBuilder.toString();
     }
 }
--- hangman/src/Main.java
+++ hangman/src/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -14,11 +11,12 @@ public class Main {
     public static void toPlay() {
         boolean correctAnswer = true;
         while (correctAnswer) {
-            System.out.println("Хотите сыграть? ('Y' - да, 'N' - нет): ");
-            String answer = scanner.next().toLowerCase();
-            if (answer.equals("y")) {
-                GameProcess.action();
-            } else if (answer.equals("n")) {
+            System.out.println("Хотите сыграть? ([Д]а, [Н]ет): ");
+            char answer = Character.toLowerCase(scanner.next().charAt(0));
+            if (answer == 'д') {
+                GameProcess gameProcess = new GameProcess();
+                gameProcess.action();
+            } else if (answer == 'н') {
                 System.out.println("До свидания");
                 correctAnswer = false;
             } else {
